4122afbeb1931c246ed6d6b705a07dfb29841b91
YueHui <527329923@qq.com>
2020-11-03 18:21:23 +0800

    Create 69.x-的平方根.js

---
+++ 69.x-的平方根.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=javascript
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let i=0;
+    while(i*i<=x){
+        i++;
+    }
+    return i-1;
+};
+// @lc code=end
+
+// console.log(mySqrt(9));
